d807b4425ad9723657cae4415eee9bc2c25e4154
Kasra Naderi <kasra@yuser.co>
2022-08-12 11:09:42 -0400

    change toHexString() to toString()

--- src/mapping.ts
+++ src/mapping.ts
@@ -11,9 +11,9 @@ import { log } from "@graphprotocol/graph-ts";
 export function handleNewCollection(event: CollectionCreated): void {
   let collection = new CollectionEntity(event.transaction.hash.toHex());
 
-  collection.collection = event.params.collection.toHexString();
+  collection.collection = event.params.collection.toString();
   collection.royalties = event.params.royalties;
-  collection.owner = event.params.owner.toHexString();
+  collection.owner = event.params.owner.toString();
   collection.collectionId = event.params.collectionId;
   Collection.create(event.params.collection);
   collection.save();
@@ -25,8 +25,8 @@ export function handleNewCollection(event: CollectionCreated): void {
 
 export function handleCollectionInitialized(event: Initialized): void {
   let initialized = new InitializedEntity(event.transaction.hash.toHex());
-  initialized.owner = event.params.owner.toHexString();
-  initialized.collection = event.address.toHexString();
+  initialized.owner = event.params.owner.toString();
+  initialized.collection = event.address.toString();
   initialized.collectionId = event.params.collectionId;
   initialized.save();
 
@@ -35,10 +35,10 @@ export function handleCollectionInitialized(event: Initialized): void {
 
 export function handleTransfer(event: Transfer): void {
   let transfer = new TransferEntity(event.transaction.hash.toHex());
-  transfer.from = event.params.from.toHexString();
-  transfer.to = event.params.to.toHexString();
+  transfer.from = event.params.from.toString();
+  transfer.to = event.params.to.toString();
   transfer.tokenId = event.params.tokenId;
-  transfer.collection = event.address.toHexString();
+  transfer.collection = event.address.toString();
   transfer.save();
 
   log.info(`Transfer event processed at address: ${event.address}`, []);
@@ -46,10 +46,10 @@ export function handleTransfer(event: Transfer): void {
 
 export function handleRoleGranted(event: RoleGranted): void {
   let roleGranted = new RoleGrantedEntity(event.transaction.hash.toHex());
-  roleGranted.role = event.params.role.toHexString(); // role has to be converted to string
-  roleGranted.account = event.params.account.toHexString();
-  roleGranted.sender = event.params.sender.toHexString();
-  roleGranted.collection = event.address.toHexString();
+  roleGranted.role = event.params.role.toString(); // role has to be converted to string
+  roleGranted.account = event.params.account.toString();
+  roleGranted.sender = event.params.sender.toString();
+  roleGranted.collection = event.address.toString();
   roleGranted.save();
 
   log.info(`RoleGranted event processed at address: ${event.address}`, []);
@@ -57,10 +57,10 @@ export function handleRoleGranted(event: RoleGranted): void {
 
 export function handleRoleRevoked(event: RoleRevoked): void {
   let roleRevoked = new RoleRevokedEntity(event.transaction.hash.toHex());
-  roleRevoked.role = event.params.role.toHexString(); // role has to be converted to string
-  roleRevoked.account = event.params.account.toHexString();
-  roleRevoked.sender = event.params.sender.toHexString();
-  roleRevoked.collection = event.address.toHexString();
+  roleRevoked.role = event.params.role.toString(); // role has to be converted to string
+  roleRevoked.account = event.params.account.toString();
+  roleRevoked.sender = event.params.sender.toString();
+  roleRevoked.collection = event.address.toString();
   roleRevoked.save();
 
   log.info(`RoleRevoked event processed at address: ${event.address}`, []);
@@ -68,11 +68,11 @@ export function handleRoleRevoked(event: RoleRevoked): void {
 
 export function handleMint(event: Mint): void {
   let mint = new MintEntity(event.transaction.hash.toHex());
-  mint.minter = event.params.minter.toHexString();
-  mint.to = event.params.to.toHexString();
+  mint.minter = event.params.minter.toString();
+  mint.to = event.params.to.toString();
   mint.tokenId = event.params.tokenId;
   mint.tokenHash = event.params.tokenHash;
-  mint.collection = event.address.toHexString();
+  mint.collection = event.address.toString();
   mint.save();
 
   log.info(`Mint event processed at address: ${event.address}`, []);
@@ -83,11 +83,11 @@ export function handleMint(event: Mint): void {
 export function handleItemListed(event: ItemListed): void {
   let itemListed = new ItemListedEntity(event.transaction.hash.toHex());
   itemListed.itemNumber = event.params.itemNumber;
-  itemListed.auctionEnd = event.params.auctionEnd.toHexString();
-  itemListed.seller = event.params.seller.toHexString();
+  itemListed.auctionEnd = event.params.auctionEnd.toString();
+  itemListed.seller = event.params.seller.toString();
   itemListed.tokenId = event.params.tokenId;
-  itemListed.saleToken = event.params.saleToken.toHexString();
-  itemListed.nftToken = event.params.nftToken.toHexString();
+  itemListed.saleToken = event.params.saleToken.toString();
+  itemListed.nftToken = event.params.nftToken.toString();
   itemListed.minPrice = event.params.minPrice;
   itemListed.save();
 
@@ -98,7 +98,7 @@ export function handleBid(event: Bid): void {
   let bid = new BidEntity(event.transaction.hash.toHex());
   bid.itemNumber = event.params.itemNumber;
   bid.bidAmount = event.params.bidAmount;
-  bid.bidder = event.params.bidder.toHexString();
+  bid.bidder = event.params.bidder.toString();
   bid.tokenId = event.params.tokenId;
   bid.save();
 
@@ -109,8 +109,8 @@ export function handleSettled(event: Settled): void {
   let settled = new SettledEntity(event.transaction.hash.toHex());
   settled.itemNumber = event.params.itemNumber;
   settled.bidAmount = event.params.bidAmount;
-  settled.winner = event.params.winner.toHexString();
-  settled.seller = event.params.seller.toHexString();
+  settled.winner = event.params.winner.toString();
+  settled.seller = event.params.seller.toString();
   settled.tokenId = event.params.tokenId;
   settled.save();
 
@@ -121,10 +121,10 @@ export function handleSettled(event: Settled): void {
 
 export function handleNextGemsTransfer(event: NextGemsTransfer): void {
   let transfer = new TransferEntity(event.transaction.hash.toHex());
-  transfer.from = event.params.from.toHexString();
-  transfer.to = event.params.to.toHexString();
+  transfer.from = event.params.from.toString();
+  transfer.to = event.params.to.toString();
   transfer.tokenId = event.params.tokenId;
-  transfer.collection = event.address.toHexString();
+  transfer.collection = event.address.toString();
   transfer.save();
 
   log.info(`Transfer event processed at address: ${event.address}`, []);
